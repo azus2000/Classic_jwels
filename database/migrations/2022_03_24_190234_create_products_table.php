5e9e0a236f24c475a2bb8e0c465ec8d8a2abda25
Ayush Ranjan <50829842+azus2000@users.noreply.github.com>
2022-04-13 01:18:13 +0530

    Add files via upload

---
+++ database/migrations/2022_03_24_190234_create_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // $table->bigInteger('category_id')->unsigned()->index();
+            // $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            // $table->unsignedBigInteger('category_id');
+            $table->foreignIdFor(\App\Models\CAtegory::class,'category_id');
+            $table->string('slug')->unique();
+            $table->string('short_description')->nullable();
+            $table->text('description');
+            $table->decimal('regular_price');
+            $table->decimal('sales_price')->nullable();
+            $table->string('SKU');
+            $table->enum('stock_status',['instock','outstock']);
+            $table->boolean('featured')->default(false);
+            $table->unsignedInteger('quantity')->default(10);
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
